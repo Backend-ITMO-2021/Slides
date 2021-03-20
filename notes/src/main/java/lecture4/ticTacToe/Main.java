1d0982c003a530a1937e06eb4e044b3100c2954a
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-03-20 20:22:12 +0200

    lecture 4

--- notes/src/main/java/lecture4/ticTacToe/Main.java
+++ notes/src/main/java/lecture4/ticTacToe/Main.java
@@ -4,10 +4,11 @@ package lecture4.ticTacToe;
 import lecture4.ticTacToe.board.TicTacToeBoard;
 import lecture4.ticTacToe.game.Game;
 import lecture4.ticTacToe.player.HumanPlayer;
+import lecture4.ticTacToe.player.RandomPlayer;
 
 public class Main {
   public static void main(String[] args) {
-    final Game game = new Game(false, new HumanPlayer(), new HumanPlayer());
+    final Game game = new Game(false, new HumanPlayer(), new RandomPlayer());
     int result;
     do {
       result = game.play(new TicTacToeBoard());
